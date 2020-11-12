af7af92b5af5cade34b66949801c420b979d0ae8
stepin105186 <69074362+stepin105186@users.noreply.github.com>
2020-11-12 10:28:35 +0530

    Update test.c

--- test/test.c
+++ test/test.c
@@ -1,73 +1,30 @@
-#include <CUnit/Basic.h>
-#include <CUnit/CUnit.h>
+#include "unity.h"
+#include "projectfirst.h"
 
-#include <guessnumber.h>
-#define PROJECT_NAME    "guessnumber"
+	void setUp()
+	{	}
+	void tearDown()
+	{	}
 
-void test_MainMenu(void);
-void test_GetChoice(void);
-void test_guessGame(void);
-void test_PrintScore(void);
-void test_Quit(void);
-void test_NumDigits(void);
-void test_numPoints(void);
+	void test_GetChoice(void)
+	{
+  		TEST_ASSERT_EQUAL(1,GetChoice(1,1));
 
-int main()
-{
-   if (CUE_SUCCESS != CU_initialize_registry())
-    return CU_get_error();
-  CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
-  
-  CU_add_test(suite, "MainMenu", test_MainMenu);
-  CU_add_test(suite, "GetChoice", test_GetChoice);
-  CU_add_test(suite, "guessGame", test_guessGame);
-  CU_add_test(suite, "PrintScore", test_PrintScore);
-  CU_add_test(suite, "Quit", test_Quit);
-  CU_add_test(suite, "NumDigits", test_NumDigits);
-  CU_add_test(suite, "numPoints", test_numPoints);
-  
-  CU_basic_set_mode(CU_BRM_VERBOSE);
-  CU_basic_run_tests();
-  CU_cleanup_registry();
-  return 0;
- }
- 
- void test_MainMenu(void)
- {
- 
- }
- 
- void test_GetChoice(void)
- {
-          CU_ASSERT(1 == GetChoice(1, 3));
-          CU_ASSERT(2 == GetChoice(1, 3)); 
-          CU_ASSERT(3 == GetChoice(1, 3));
- }
- 
- void test_guessGame(void)
- {
-          CU_ASSERT(5== guessGame());
-          CU_ASSERT(1== guessGame());
-          CU_ASSERT(2== guessGame());
-          CU_ASSERT(3== guessGame());
-          CU_ASSERT(4== guessGame());
-          CU_ASSERT(5== guessGame());
+	}
 
- }
- 
- void test_PrintScore(void)
- {
- }
- 
- void test_Quit(void)
-{
-}
- 
- void test_NumDigits(void)
- {
-           
- }
- 
- void test_numPoints(void)
- {
- }
+	void test_guessGame(void)
+	{
+  		TEST_ASSERT_EQUAL(1,guessGame(1,1));
+
+	}
+
+
+	int test_main(void)
+	{
+	  	UNITY_BEGIN();
+
+	  	RUN_TEST(test_GetChoice);
+	  	RUN_TEST(test_guessGame);
+
+  		return UNITY_END();
+	}
