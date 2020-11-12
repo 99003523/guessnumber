34eeb95bd9ac90aa435d765a43c2d0306bf8b904
stepin105186 <69074362+stepin105186@users.noreply.github.com>
2020-11-12 10:47:53 +0530

    Update and rename guessnumber.h to projectfirst.h

--- inc/projectfirst.h
+++ inc/projectfirst.h
@@ -1,6 +1,10 @@
 #ifndef PROJECTFIRST_H
 #define PROJECTFIRST_H
 
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 void MainMenu();
 int GetChoice(int min,int max);
 double guessGame( );
