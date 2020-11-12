49dd216ce04234b900139ccc6fbc5ec313d3941e
stepin105186 <69074362+stepin105186@users.noreply.github.com>
2020-11-12 10:27:41 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
-#include "guessnumber.h"
-
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "projectfirst.h"
+#include "test_projectfirst.h"
 int main()
 {
     int Choice;
@@ -7,22 +10,22 @@ int main()
     Choice = GetChoice(1,3);
     while(1)
     {
-        if(Choice==1)
+        if(Choice == 1)
         {
             guessGame();
             PrintScore();
         }
-        else if(Choice==2)
+        else if (Choice == 2)
         {
             PrintScore();
         }
-        else if(Choice==3)
+        else if (Choice == 3)
         {
             Quit();
         }
         MainMenu();
         Choice=GetChoice(1,3);
     }
- 
+    test_main();
     return 0;
 }
